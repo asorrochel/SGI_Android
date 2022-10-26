795fb523e39b1c31e4ea7cfc901d792006ca4aed
Adrian Sorroche <asorrocheL@protonmail.com>
2022-10-27 01:19:46 +0200

    interfaz Registro implementada

---
+++ app/src/main/java/com/example/sgi/registroActivity.java
@@ -0,0 +1,14 @@
+package com.example.sgi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
