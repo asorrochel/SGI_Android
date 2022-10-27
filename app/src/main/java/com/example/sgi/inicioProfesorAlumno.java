2320fef8bad9d52b49f4ce19696ebfd477fe0fa6
Adrian Sorroche <asorrocheL@protonmail.com>
2022-10-27 12:16:48 +0200

    interfaz inicio profesor y inicio alumno implementadas

---
+++ app/src/main/java/com/example/sgi/inicioProfesorAlumno.java
@@ -0,0 +1,14 @@
+package com.example.sgi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inicioProfesorAlumno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_profesor_alumno);
+    }
+}
