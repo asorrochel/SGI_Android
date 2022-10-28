48c2ee52623c893b94bb618c30e766de42316b3e
Adrian Sorroche <asorrocheL@protonmail.com>
2022-10-28 11:28:17 +0200

    implementacion Registro Material Design

--- app/src/main/java/com/example/sgi/registro.java
+++ app/src/main/java/com/example/sgi/registro.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class registroActivity extends AppCompatActivity {
+public class registro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
