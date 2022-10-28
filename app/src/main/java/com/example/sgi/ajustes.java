706082a13855d0f96adc7175991b53191cbc3216
Adrian Sorroche <asorrocheL@protonmail.com>
2022-10-28 15:39:18 +0200

    implementacio ajustes Material Design

--- app/src/main/java/com/example/sgi/ajustes.java
+++ app/src/main/java/com/example/sgi/ajustes.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ajustesActivity extends AppCompatActivity {
+public class ajustes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
