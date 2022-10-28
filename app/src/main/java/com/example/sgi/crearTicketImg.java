08a860cb0173209af3b56937a8a3c7fb4cd5bf53
Adrian Sorroche <asorrocheL@protonmail.com>
2022-10-28 15:21:30 +0200

    interfaz crear Ticket Material Design implementada

---
+++ app/src/main/java/com/example/sgi/crearTicketImg.java
@@ -0,0 +1,30 @@
+package com.example.sgi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class crearTicketImg extends AppCompatActivity {
+
+    TextInputLayout textInputLayout;
+    AutoCompleteTextView autoCompleteTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_ticket_img);
+
+        textInputLayout = findViewById(R.id.crear_ticket_aula_img_dropdown);
+        autoCompleteTextView = findViewById(R.id.autocomplete_aula_img);
+
+        String [] aulas = new String[]{"C01 Diurno","C01 Vespertino","C02 Diurno","C02 Vespertino","C03 Diurno","C03 Vespertino","C04 Diurno","C04 Vespertino","C05 Diurno","C05 Vespertino","C06 Diurno","C06 Vespertino","C07 Diurno","C07 Vespertino","C08 Diurno","C08 Vespertino","C09 Diurno","C09 Vespertino","C10 Diurno","C10 Vespertino","C11 Diurno","C11 Vespertino","C12 Diurno","C12 Vespertino","C13 Diurno","C13 Vespertino","Taller"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                crearTicketImg.this,R.layout.dropdowm_item,aulas);
+
+        autoCompleteTextView.setAdapter(adapter);
+    }
+}
