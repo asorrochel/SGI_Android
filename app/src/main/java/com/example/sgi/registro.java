bc92037844c5ef4c52e696ac04623c16e943b6f5
JruizdGaona <javierruizdegaona@gmail.com>
2022-11-02 18:34:46 +0100

    Prueba

--- app/src/main/java/com/example/sgi/registro.java
+++ app/src/main/java/com/example/sgi/registro.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
