76efcc3da1e71b49458281aca94ad8e81fdc66c2
JruizdGaona <javierruizdegaona@gmail.com>
2022-11-10 21:28:39 +0100

    Funcionalidad crearTicketImg y crearTciket

--- app/src/main/java/com/example/sgi/crearTicket/crearTicket.java
+++ app/src/main/java/com/example/sgi/crearTicket/crearTicket.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.sgi.R;
+import com.example.sgi.inicio.inicioProfesorAlumno;
+import com.example.sgi.inicio.inicioProfesoresMnt;
+import com.example.sgi.inicio.inicioTutores;
+import com.example.sgi.inicio.inicioTutoresMnt;
+import com.example.sgi.utils.Usuario;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class crearTicket extends AppCompatActivity {
@@ -19,6 +29,7 @@ public class crearTicket extends AppCompatActivity {
     Toolbar toolbar;
     TextInputLayout textInputLayout;
     AutoCompleteTextView autoCompleteTextView;
+    Button botonEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +37,9 @@ public class crearTicket extends AppCompatActivity {
         setContentView(R.layout.activity_crear_ticket);
 
         toolbar = findViewById(R.id.mainToolBar);
-        setToolbar(toolbar);
-
         textInputLayout = findViewById(R.id.crear_ticket_aula_dropdown);
         autoCompleteTextView = findViewById(R.id.autocomplete_aula);
+
 
         String [] aulas = new String[]{"C01 Diurno","C01 Vespertino","C02 Diurno","C02 Vespertino","C03 Diurno","C03 Vespertino","C04 Diurno","C04 Vespertino","C05 Diurno","C05 Vespertino","C06 Diurno","C06 Vespertino","C07 Diurno","C07 Vespertino","C08 Diurno","C08 Vespertino","C09 Diurno","C09 Vespertino","C10 Diurno","C10 Vespertino","C11 Diurno","C11 Vespertino","C12 Diurno","C12 Vespertino","C13 Diurno","C13 Vespertino","Taller"};
 
@@ -37,6 +47,26 @@ public class crearTicket extends AppCompatActivity {
                 crearTicket.this,R.layout.dropdowm_item,aulas);
 
         autoCompleteTextView.setAdapter(adapter);
+
+        setToolbar(toolbar);
+        clickBotonEnviar();
+    }
+
+    private void clickBotonEnviar() {
+        botonEnviar.setOnClickListener((View) -> {
+            new MaterialAlertDialogBuilder(this)
+                    .setTitle("Crear Ticket")
+                    .setMessage("¿Desea crear el Ticket?")
+                    .setPositiveButton("Crear", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(crearTicket.this, inicioProfesorAlumno.class));
+                            Toast.makeText(crearTicket.this, "Ticket creado correctamente", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton("Cancelar", null)
+                    .show();
+        });
     }
 
     private void setToolbar(Toolbar toolbar) {
